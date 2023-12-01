55df9e4360677f8b255364e909c14e24ac2b7414
Diego Herrera <daherre0@gmail.com>
2023-12-01 14:15:55 -0500

    Update Navbar styles and add inactive chevron text

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,11 +3,11 @@ import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 const Navbar = () => {
   return (
     <nav className="debug bg-spotify-black flex items-center justify-between py-4 px-5 rounded-lg">
-      <div className="flex gap-4 text-xl text-spotify-chevron-gray">
-        <div className="bg-spotify-chevron-black p-1 rounded-full">
+      <div className="flex gap-4 text-xl text-spotify-gray2">
+        <div className="bg-spotify-chevron-black p-1 rounded-full inactive-chevron-text ">
           <BsChevronLeft />
         </div>
-        <div className="bg-spotify-chevron-black p-1 rounded-full">
+        <div className="bg-spotify-chevron-black p-1 rounded-full inactive-chevron-text ">
           <BsChevronRight />
         </div>
       </div>
