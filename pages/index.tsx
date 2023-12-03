f54f09df0f3960a32ebf5705db1cb1faf14cf798
Diego Herrera <daherre0@gmail.com>
2023-12-03 17:50:26 -0500

    Add TestDialog and TestDialogContainer components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import { Navbar } from "@/components/Navbar";
 import { PlaylistContainer } from "@/components/PlaylistContainer";
 import { Sidebar } from "@/components/Sidebar";
+import { TestDialogContainer } from "@/components/TestDialogContainer";
 
 const Home = () => {
   return (
     <div className=" bg-black h-screen flex text-white">
       <Sidebar />
-      <main className="debug w-full p-2">
+      <main className="debug w-full p-2 h-full flex flex-col" >
         <Navbar />
-        <div className="px-5 py-1 bg-gradient-to-br from-gradient-gray to-black">
+        <div className="px-5 py-1 bg-gradient-to-br from-gradient-gray to-black h-full flex flex-col rounded-md gap-4">
           <PlaylistContainer title="Spotify Playlists" />
           <PlaylistContainer title="Great First Audiobooks" />
         </div>
 
-        <footer></footer>
+        <footer>
+          <TestDialogContainer />
+        </footer>
       </main>
     </div>
   );
